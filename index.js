3e536f0df648a0a432871f88427b48a8d57524e4
balaji.ramesh <balaji.ramesh@zohocorp.com>
2024-05-20 10:10:21 +0530

    v0.0.1

---
+++ index.js
@@ -0,0 +1,59 @@
+
+function initBody() {
+	document.querySelector('#container').innerHTML = `<h1>ESP32 Web Server</h1>
+	<h3>Using Access Point(AP) Mode</h3>
+	<div data-led="1">
+		<p>LED1 Status: <span>OFF</span></p>
+		<button class="button" onclick="setLED(this)">ON</button>
+	</div>
+	<div data-led="2">
+		<p>LED2 Status: <span>OFF</span></p>
+		<button class="button" onclick="setLED(this)">ON</button>
+	</div>
+	<br>
+	<button class="button" onclick="refreshStatus()">Refresh</button>`;
+}
+initBody();
+
+function setLED(ledNumber, btn) {
+	const isOn = btn.classList.contains('status-on');
+	const url = `/led/${ledNumber}/${isOn ? 'off' : 'on'}`;
+	fetch(url, {
+		method: 'POST'
+	})
+		.then(response => {
+			if (response.ok) {
+				const status = {};
+				status[`led${ledNumber}`] = isOn;
+				updateStatus(status);
+			} else {
+				alert('Problem Occurred!');
+			}
+		});
+}
+function refreshStatus() {
+	fetch('/led', {
+		method: 'GET'
+	})
+		.then(response => response.json())
+		.then(function (json) {
+			updateStatus(json);
+		});
+}
+
+function updateStatus(json) {
+	if (json) {
+		[1, 2].forEach(num => {
+			if (json.hasOwnProperty(`led${num}`)) {
+				document.querySelector(`[data-led="${num}"] span`).textContent = json[`led${num}`] ? 'OFF' : 'ON';
+				if (json[`led${num}`]) {
+					document.querySelector(`[data-led="${num}"] button`).classList.add('status-on');
+				} else {
+					document.querySelector(`[data-led="${num}"] button`).classList.remove('status-on');
+				}
+			}
+		});
+	}
+}
+
+refreshStatus();
--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,17 @@ IPAddress subnet(255, 255, 255, 0);
 WebServer server(8118);
 
 bool LED1status = LOW;
-
 bool LED2status = LOW;
 
-String SendHTML(uint8_t led1stat, uint8_t led2stat);
+String SendHTML(bool isNotFound = false);
+String SendJSON(bool led1, bool led2);
+
 void handle_OnConnect();
 void handle_led1on();
 void handle_led1off();
 void handle_led2on();
 void handle_led2off();
+void handle_Status();
 void handle_NotFound();
 
 void setup()
@@ -34,10 +36,11 @@ void setup()
 	delay(100);
 
 	server.on("/", handle_OnConnect);
-	server.on("/led1on", handle_led1on);
-	server.on("/led1off", handle_led1off);
-	server.on("/led2on", handle_led2on);
-	server.on("/led2off", handle_led2off);
+	server.on("/led", handle_Status);
+	server.on("/led/1/on", handle_led1on);
+	server.on("/led/1/off", handle_led1off);
+	server.on("/led/2/on", handle_led2on);
+	server.on("/led/2/off", handle_led2off);
 	server.onNotFound(handle_NotFound);
 
 	server.begin();
@@ -77,83 +80,75 @@ void loop()
 
 void handle_OnConnect()
 {
-	LED1status = LOW;
-	LED2status = LOW;
-	Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-	server.send(200, "text/html", SendHTML(LED1status, LED2status));
+	server.send(200, "text/html", SendHTML());
+}
+
+void handle_Status()
+{
+	server.send(200, "text/json", SendJSON(LED1status, LED2status));
 }
 
 void handle_led1on()
 {
 	LED1status = HIGH;
 	Serial.println("GPIO4 Status: ON");
-	server.send(200, "text/html", SendHTML(true, LED2status));
+	server.send(204);
 }
 
 void handle_led1off()
 {
 	LED1status = LOW;
 	Serial.println("GPIO4 Status: OFF");
-	server.send(200, "text/html", SendHTML(false, LED2status));
+	server.send(204);
 }
 
 void handle_led2on()
 {
 	LED2status = HIGH;
 	Serial.println("GPIO5 Status: ON");
-	server.send(200, "text/html", SendHTML(LED1status, true));
+	server.send(204);
 }
 
 void handle_led2off()
 {
 	LED2status = LOW;
 	Serial.println("GPIO5 Status: OFF");
-	server.send(200, "text/html", SendHTML(LED1status, false));
+	server.send(204);
 }
 
 void handle_NotFound()
 {
-	server.send(404, "text/plain", "Not found");
+	server.send(404, "text/html", SendHTML(true));
 }
 
-String SendHTML(uint8_t led1stat, uint8_t led2stat)
+String SendHTML(bool isNotFound)
 {
-	String ptr = "<!DOCTYPE html> <html>\n";
-	ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-	ptr += "<title>LED Control</title>\n";
-	ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-	ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-	ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-	ptr += ".button-on {background-color: #3498db;}\n";
-	ptr += ".button-on:active {background-color: #2980b9;}\n";
-	ptr += ".button-off {background-color: #34495e;}\n";
-	ptr += ".button-off:active {background-color: #2c3e50;}\n";
-	ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-	ptr += "</style>\n";
-	ptr += "</head>\n";
-	ptr += "<body>\n";
-	ptr += "<h1>ESP32 Web Server</h1>\n";
-	ptr += "<h3>Using Access Point(AP) Mode</h3>\n";
+	String htmlContent = "<!DOCTYPE html>";
+	htmlContent += "<html>";
+	htmlContent += "<head>";
+		htmlContent += "<meta charset='utf-8'>";
+		htmlContent += "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+		htmlContent += "<title>Balaji ESP32</title>";
+		htmlContent += "<link href='https://cdn.jsdelivr.net/gh/IoTBalaji/esp32@v0/index.css' rel='stylesheet' crossorigin='anonymous'>";
+	htmlContent += "</head>";
+	htmlContent += "<body>";
 
-	if (led1stat)
-	{
-		ptr += "<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
+	if (isNotFound) {
+		htmlContent += "<div id=\"container\">";
+			htmlContent += "<h1>Page Not Found</h1>";
+			htmlContent += "<a href=\"/\">Go Back</a>";
+		htmlContent += "</div>";
+	} else {
+		htmlContent += "<div id=\"container\">";
+			htmlContent += "<h1>Poor Internet Connection!</h1>";
+		htmlContent += "</div>";
+		htmlContent += "<script src='https://cdn.jsdelivr.net/gh/IoTBalaji/esp32@v0/index.js' crossorigin='anonymous'></script>";
 	}
-	else
-	{
-		ptr += "<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
-	}
+	htmlContent += "</div>";
+	htmlContent += "</html>";
+	return htmlContent;
+}
 
-	if (led2stat)
-	{
-		ptr += "<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";
-	}
-	else
-	{
-		ptr += "<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";
-	}
-
-	ptr += "</body>\n";
-	ptr += "</html>\n";
-	return ptr;
+String SendJSON(bool led1, bool led2) {
+	return String("{\"led1\":" + String(led1 == true ? "true" : "false") + ", \"led2\":" + String(led2 == true ? "true" : "false") + "}");
 }
